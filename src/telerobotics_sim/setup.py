6c9006ad67994c3cd802093f3c8e1a13f2a35d0f
Grant Riddle <grantdridde7@gmail.com>
2026-03-30 14:37:43 -0600

    git in wsl doesn't work so good with vscode

---
+++ src/telerobotics_sim/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+package_name = 'telerobotics_sim'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='grant',
+    maintainer_email='grantdridde7@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'mujoco = telerobotics_sim.mujoco_node:main',
+            'listener = telerobotics_sim.position_listener:main',
+            'control = telerobotics_sim.control_publisher:main',
+        ],
+    },
+)
